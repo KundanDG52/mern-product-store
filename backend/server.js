038b5d1b83a9e02ba771233a65711ee0406876af
Kundan Gahalot <gahalotkundan@gmail.com>
2025-08-12 11:27:25 +0530

    server backend and db setup

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
 // GET
-app.get("/", (req, res) => {
-  res.send("Server is ready");
-});
+app.get("/", (req, res) => {});
 
 app.listen(5000, () => {
+  connectDB();
   console.log("Server started on port http://localhost:5000");
 });
